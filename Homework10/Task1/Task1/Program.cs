0240a1810c622a929ad0b91b6f005fb7a0607d4e
Mariam Zenaishvili <mariamz@theneo.io>
2023-05-27 16:02:27 +0400

    added files

---
+++ Homework10/Task1/Task1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+
+namespace HashTableExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 14.1.5
+            Hashtable hashTable = new Hashtable(10); 
+
+            // fruits and amounts of them 
+            hashTable.Add("Apple", "9");
+            hashTable.Add("Orange", "3");
+
+            // Displaying the number of elements in the hash table
+            Console.WriteLine("Number of Elements: " + hashTable.Count);
+            Console.WriteLine();
+
+            // Displaying all keys and values in the hash table
+            Console.WriteLine("Keys: ");
+            foreach (var key in hashTable.Keys)
+            {
+                Console.WriteLine(key);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Values: ");
+            foreach (var value in hashTable.Values)
+            {
+                Console.WriteLine(value);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
