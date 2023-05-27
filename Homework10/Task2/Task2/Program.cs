5224016af0e9d50b54aeb7d60bbaaf43ed0687c0
Mariam Zenaishvili <mariamz@theneo.io>
2023-05-27 16:29:41 +0400

    added task2

---
+++ Homework10/Task2/Task2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack stack = new Stack();
+
+            for (int i = 1; i <= 10; i++)
+            {
+                stack.Push($"Element {i}");
+            }
+
+            if (stack.Count > 0)
+            {
+                string lastElement = stack.Peek().ToString();
+                Console.WriteLine("Last Element: " + lastElement);
+            }
+            else
+            {
+                Console.WriteLine("Stack is empty");
+            }
+
+            Console.WriteLine("Number of Elements: " + stack.Count);
+
+            // Displaying all the elements in the stack
+            Console.WriteLine("Elements in the Stack:");
+            foreach (var element in stack)
+            {
+                Console.WriteLine(element);
+            }
+
+            stack.Clear();
+
+            Console.ReadLine();
+        }
+    }
+}
